512c8d4b679404967fe83cefd189be225c784931
German Cruz <cruzgerman216@yahoo.com>
2020-05-18 23:17:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,12 @@ class ApplicationController < Sinatra::Base
     redirect "/articles/#{@article.id}"
   end
 
+  get 'articles' do
+    @articles = Article.all
+    erb :index
+  end
   get '/articles/:id' do
     erb :"../views/show"
   end
+  
 end
