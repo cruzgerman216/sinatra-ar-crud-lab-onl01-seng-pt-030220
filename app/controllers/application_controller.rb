ba1acfe2fcff4afaebe094c9e825fbe018877144
German Cruz <cruzgerman216@yahoo.com>
2020-05-19 00:11:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     new_article = {}
     new_article[:title] = params[:title]
     new_article[:content] = params[:content]
-    article.update(new_article)
+    article.update(params)
     redirect "/articles/#{id}"
   end
 end
